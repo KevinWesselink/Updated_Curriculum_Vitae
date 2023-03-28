a7fe5698e7580634a46205722ba0c7f2117fc232
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-28 10:52:02 +0200

    New views and layouts
    Nieuwe views en layouts aangemaakt.

--- Updated_Curriculum_Vitae/routes/web.php
+++ Updated_Curriculum_Vitae/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CurriculumVitaeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Common Resource Routes:
+// index - Show all listings
+// show - Show single listing
+// create - Show form to create new listing
+// store - Store new listing
+// edit - Show form to edit listing
+// update - Update listing
+// destroy - Delete listing
+
 // View homepage
 Route::get('/', [CurriculumVitaeController::class, 'index']);
 
 // View registration form
-
+Route::get('/register', [UserController::class, 'create']);
 
 // View login form
+Route::get('/login', [UserController::class, 'login']);
