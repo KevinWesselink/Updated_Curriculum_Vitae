70dd05e858fb5cbd6d10b5019ba2672b090f5e78
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-30 10:30:56 +0200

    Update index.blade.php
    Veranderingen aangebracht in index.blade.php.

--- Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
+++ Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
@@ -3,7 +3,9 @@
     @include('partials._search')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 max-4">
 
-        @unless(count($Experience) == 0)
+        {{-- {{var_dump($Experience)}} --}}
+
+        @unless($Experience->count() == 0)
 
             @foreach ($Experience as $Experience)
                 {{-- <x-listing-card :listing="$listing" /> --}}
@@ -12,7 +14,7 @@
             <p>Geen werkervaringen gevonden.</p>
         @endunless
 
-        @unless(count($Education) == 0)
+        @unless($Education->count() == 0)
 
             @foreach ($Education as $Education)
                 {{-- <x-listing-card :listing="$listing" /> --}}
@@ -21,7 +23,7 @@
             <p>Geen opleidingen gevonden.</p>
         @endunless
 
-        @unless(count($Courses) == 0)
+        @unless($Courses->count() == 0)
 
             @foreach ($Courses as $Courses)
                 {{-- <x-listing-card :listing="$listing" /> --}}
@@ -32,7 +34,7 @@
 
     </div>
 
-    {{-- <div class="mt-6 p-4">
-        {{ $listings->links() }}
-    </div> --}}
+    <div class="mt-6 p-4">
+        {{-- {{ $listings->links() }} --}}
+    </div>
 </x-layout>
