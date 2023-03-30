d49da5668ea8430a0616422d8cee2a47b22373ea
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-30 15:58:35 +0200

    Debug
    DD's toegevoegd in meerdere bestanden.

--- Updated_Curriculum_Vitae/app/Http/Controllers/CurriculumVitaeController.php
+++ Updated_Curriculum_Vitae/app/Http/Controllers/CurriculumVitaeController.php
@@ -11,6 +11,7 @@ class CurriculumVitaeController extends Controller
     // Show homepage
     public function index() {
             $Experience = Experience::all();
+            // dd($Experience);
             $Education = Education::all();
             $Courses = Courses::all();
             return view('/curriculumvitae.index')->with('Experience', $Experience)->with('Education', $Education)->with('Courses', $Courses);
