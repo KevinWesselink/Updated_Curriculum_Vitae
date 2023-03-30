b0109fe8c11bd31e254b33fd2ee8794a27573d22
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-30 08:37:12 +0200

    Update CurriculumVitaeController
    Function geüpdatet.

--- Updated_Curriculum_Vitae/app/Http/Controllers/CurriculumVitaeController.php
+++ Updated_Curriculum_Vitae/app/Http/Controllers/CurriculumVitaeController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Courses;
+use App\Models\Education;
+use App\Models\Experience;
 
 class CurriculumVitaeController extends Controller
 {
     // Show homepage
     public function index() {
-        return view('curriculumvitae.index');
+        return view('curriculumvitae.index', [
+            'Education' => Education::latest(),
+            'Experience' => Experience::latest(),
+            'Courses' => Courses::latest()
+        ]);
     }
 }
