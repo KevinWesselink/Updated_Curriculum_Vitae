73d3605edeabce20439827c01937886a72fcf70b
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-30 08:36:48 +0200

    Homepage update
    Homepage geüpdatet.

--- Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
+++ Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
@@ -3,18 +3,36 @@
     @include('partials._search')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 max-4">
 
-        @unless(count($listings) == 0)
+        @unless(count($Experience) == 0)
 
-            @foreach ($listings as $listing)
+            @foreach ($Experience as $Experience)
                 {{-- <x-listing-card :listing="$listing" /> --}}
             @endforeach
         @else
-            <p>No listings found</p>
+            <p>Geen werkervaringen gevonden.</p>
+        @endunless
+
+        @unless(count($Education) == 0)
+
+            @foreach ($Education as $Education)
+                {{-- <x-listing-card :listing="$listing" /> --}}
+            @endforeach
+        @else
+            <p>Geen opleidingen gevonden.</p>
+        @endunless
+
+        @unless(count($Courses) == 0)
+
+            @foreach ($Courses as $Courses)
+                {{-- <x-listing-card :listing="$listing" /> --}}
+            @endforeach
+        @else
+            <p>Geen cursussen gevonden.</p>
         @endunless
 
     </div>
 
-    <div class="mt-6 p-4">
-        {{$listings->links()}}
-    </div>
+    {{-- <div class="mt-6 p-4">
+        {{ $listings->links() }}
+    </div> --}}
 </x-layout>
