24237d9cc5a68f7adffe0ebc88e2a641df854a79
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-30 16:35:19 +0200

    Cards created
    Meerdere cards aangemaakt.

---
+++ Updated_Curriculum_Vitae/resources/views/components/education-card.blade.php
@@ -0,0 +1,16 @@
+@props(['education'])
+
+<x-card>
+    <div class="flex">
+        <div>
+            <h3 class="text-2xl">
+                <a href="/education/{{ $education->id }}">{{ $education->schoolName }}</a>
+            </h3>
+            <div class="text-xl font-bold mb-4">{{ $education->educationName }}</div>
+            <div class="text-xl font-bold mb-4">{{ $education->yearsFollowed }}</div>
+            <div class="text-lg mt-4">
+                <i class="fa-solid-fa-location-dot">{{ $education->schoolLocation }}</i>
+            </div>
+        </div>
+    </div>
+</x-card>
