f5b9a2a4807261bdf3478948dc26f3867cdc2cb4
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-31 12:24:52 +0200

    Update layout.blade.php
    Linkje geüpdatet.

--- Updated_Curriculum_Vitae/resources/views/components/layout.blade.php
+++ Updated_Curriculum_Vitae/resources/views/components/layout.blade.php
@@ -67,7 +67,7 @@
     <footer
         class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
         <p class="ml-2">Copyright &copy; 2023, All Rights Reserved</p>
-        <a href="/listings/create" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Update CV</a>
+        <a href="/choice" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Update CV</a>
     </footer>
 
     <x-flash-message />
