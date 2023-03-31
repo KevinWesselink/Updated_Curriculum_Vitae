85080459b16fa81bfdd63b4390482d2633211690
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-03-31 09:23:50 +0200

    Update UserController.php
    UserController geüpdatet.

--- Updated_Curriculum_Vitae/app/Http/Controllers/UserController.php
+++ Updated_Curriculum_Vitae/app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -11,8 +13,54 @@ class UserController extends Controller
         return view('users.register');
     }
 
+    // Create New User
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        // Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+        // Create User
+        $user = User::create($formFields);
+
+        // Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'Gebruiker aangemaakt en ingelogd');
+    }
+
+    // Logout User
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'Gebruiker uitgelogd');
+    }
+
     // Show Login Form
     public function login() {
         return view('users.login');
+    }
+
+    // Authenticate User
+    public function authenticate(Request $request) {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'Je bent nu ingelogd');
+        }
+
+        return back()->withErrors(['email' => 'Ongeldige Gegevens'])->onlyInput('email');
     }
 }
