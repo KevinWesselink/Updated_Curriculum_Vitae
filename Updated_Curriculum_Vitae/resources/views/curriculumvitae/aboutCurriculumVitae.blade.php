1c087250f8dade624c2b767745e7f4cc81d7b541
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-04-12 15:18:16 +0200

    Update aboutCurriculumVitae.blade.php
    Pagina aangemaakt en aangepast.

--- Updated_Curriculum_Vitae/resources/views/curriculumvitae/aboutCurriculumVitae.blade.php
+++ Updated_Curriculum_Vitae/resources/views/curriculumvitae/aboutCurriculumVitae.blade.php
@@ -1,1 +1,83 @@
-AboutCurriculumVitae
+<x-layout>
+    <x-card>
+        <!-- component -->
+        <html>
+
+        <head>
+            <link rel="stylesheet"
+                href="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/css/main.ad49aa9b.css" />
+        </head>
+
+        <body>
+            <div class="flex flex-col justify-center items-center h-[100vh]">
+                <div
+                    class="relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none p-3">
+                    <div class="mt-2 mb-8 w-full">
+                        <h4 class="px-2 text-xl font-bold text-navy-700 dark:text-white">
+                            Informatie over deze website
+                        </h4>
+                        <p class="mt-2 px-2 text-base text-gray-600">
+                            Voor een schoolopdracht heb ik in oktober een eigen CV pagina aangemaakt en live gezet. Dit deed ik om te oefenen met HTML/CSS en Hosting. 
+                            Nu ik verder ben met de opleiding en meer kennis heb van O.A. HTML/CSS en andere talen en frameworks, wilde ik mijn CV pagina updaten.
+                            Het leek me alleen een stuk makkelijker om een nieuwe website te maken, zodat ik kon oefenen met de opgedane kennis.
+                            
+                            Dit heb ik nu dus gedaan. De pagina is nog lang niet af, maar het is in een goed genoege staat om online te zetten.
+                            Ik was ook van mening dat het goed is om te laten zien wat ik allemaal kan en dit online te zetten.
+                        </p>
+                    </div>
+                    <div class="grid grid-cols-2 gap-4 px-2 w-full">
+                        <div
+                            class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Versie</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                Versie 13 april 2023
+                            </p>
+                        </div>
+
+                        <div
+                            class="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Gebruikte talen</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                Laravel, MySQL, Tailwind
+                            </p>
+                        </div>
+
+                        <div
+                            class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Duur van bouw website</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                27 maart 2023 - 13 april 2023
+                            </p>
+                        </div>
+
+                        <div
+                            class="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Eerdere CV gemaakt met</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                HTML/CSS
+                            </p>
+                        </div>
+
+                        <div
+                            class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Organisatie</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                Deltion College
+                            </p>
+                        </div>
+
+                        <div
+                            class="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
+                            <p class="text-sm text-gray-600">Aantal jaren codeerervaring</p>
+                            <p class="text-base font-medium text-navy-700 dark:text-white">
+                                3 jaar
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </body>
+
+        </html>
+    </x-card>
+</x-layout>
