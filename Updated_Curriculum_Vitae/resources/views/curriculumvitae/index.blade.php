70f0804336dc8f929659371ddba32eecbb4ab680
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-04-17 11:03:42 +0200

    Update index.blade.php
    Nieuwe tussenkopjes toegevoegd aan de code.

--- Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
+++ Updated_Curriculum_Vitae/resources/views/curriculumvitae/index.blade.php
@@ -1,9 +1,8 @@
 <x-layout>
-    
     @include('partials._hero')
     <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 max-4">
+        <h1 class="text-xl p-2 font-bold col-span-2">Werkervaringen</h1>
         @unless($Experience->count() == 0)
-
             @foreach ($Experience as $Experience)
                 <x-experience-card :experience="$Experience" />
             @endforeach
@@ -11,8 +10,8 @@
             <p>Geen werkervaringen gevonden.</p>
         @endunless
 
+        <h1 class="text-xl p-2 font-bold col-span-2">Opleidingen</h1>
         @unless($Education->count() == 0)
-
             @foreach ($Education as $Education)
                 <x-education-card :education="$Education" />
             @endforeach
@@ -20,18 +19,13 @@
             <p>Geen opleidingen gevonden.</p>
         @endunless
 
+        <h1 class="text-xl p-2 font-bold col-span-2">Cursussen</h1>
         @unless($Courses->count() == 0)
-
             @foreach ($Courses as $Courses)
                 <x-courses-card :courses="$Courses" />
             @endforeach
         @else
             <p>Geen cursussen gevonden.</p>
         @endunless
-
-    </div>
-
-    <div class="mt-6 p-4">
-        {{-- {{ $listings->links() }} --}}
     </div>
 </x-layout>
