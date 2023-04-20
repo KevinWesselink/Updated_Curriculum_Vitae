06f1b92d1159fc11eaf0fdec9216c9ef5b9e459d
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-04-20 09:47:08 +0200

    Update education-card.blade.php
    Status toegevoegd aan opleidingkaart

--- Updated_Curriculum_Vitae/resources/views/components/education-card.blade.php
+++ Updated_Curriculum_Vitae/resources/views/components/education-card.blade.php
@@ -7,7 +7,8 @@
                 <a href="/education/{{ $education->id }}">{{ $education->schoolName }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{ $education->educationName }}</div>
-            <div class="text-xl font-bold mb-4">{{ $education->yearsFollowed }}</div>
+            <div class="text-xl font-bold">{{ $education->yearsFollowed }}</div>
+            <div class="text-xl font-bold mb-4">Status: {{ $education->status }}</div>
             <div class="text-lg mt-4">
                 <i class="fa-solid-fa-location-dot">{{ $education->schoolLocation }}</i>
             </div>
