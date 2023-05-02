63c046dda6622eabd6345974f17fca37c06c3829
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-05-02 15:40:43 +0200

    Rename Files
    Meerdere bestanden een nieuwe naam gegeven.

---
+++ Updated_Curriculum_Vitae/app/Models/Programming.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Programming extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['languageName', 'experienceLevel', 'smallExplanation1', 'smallExplanation2', 'smallExplanation3', 'smallExplanation4', 'smallExplanation5', 'startedWith', 'workedWithUntil', 'user_id'];
+
+    protected $table = "programming";
+}
---
+++ Updated_Curriculum_Vitae/app/Models/SoftSkills.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SoftSkills extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['skillName', 'experienceLevel', 'smallExplanation1', 'smallExplanation2', 'smallExplanation3', 'smallExplanation4', 'smallExplanation5', 'startedWith', 'workedWithUntil', 'user_id'];
+
+    protected $table = "softskills";
+}
